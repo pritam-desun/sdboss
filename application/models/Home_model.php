7b8f233f1c4080631225c2d0e544b026370e5868
desunbera <joy.desuntech@gmail.com>
2023-03-30 15:52:46 +0530

    Template cutting For home page

---
+++ application/models/Home_model.php
@@ -0,0 +1,84 @@
+<?php
+
+
+class Home_model extends CI_Model
+{
+	public function getActiveAccountList()
+	{
+		$user_type = $this->session->user['user_type'];
+
+		if ($user_type == 1 || $user_type == 2) {
+			$user_id = $this->uri->segment(3);
+			$this->db->select('customers.id, customers.cust_code, wallet.amount as wallet_balance, dealer.full_name as dealer_name, distributor.full_name as distributor_name, customers.full_name, customers.mobile')
+				->from('customers')
+				->join('wallet', 'customers.cust_code = wallet.cust_code', 'left')
+				->join('users as dealer', 'dealer.user_id=customers.assigned_by_id', 'left')
+				->join('users as distributor', 'distributor.user_id=dealer.assigned_by_id', 'left')
+				->where('customers.is_deleted', '0')
+				->where('customers.assigned_by_id', $user_id)
+				->order_by('customers.id', 'desc');
+		} else if ($user_type == 3) { // Distributor
+			$this->db->select('customers.id, customers.cust_code, wallet.amount as wallet_balance, dealer.full_name as dealer_name, distributor.full_name as distributor_name, customers.full_name, customers.mobile')
+				->from('customers')
+				->join('wallet', 'customers.cust_code = wallet.cust_code', 'left')
+				->join('users as dealer', 'dealer.user_id=customers.assigned_by_id', 'left')
+				->join('users as distributor', 'distributor.user_id=dealer.assigned_by_id', 'left')
+				->where('customers.is_deleted', '0')
+				->order_by('customers.id', 'desc');
+		} else if ($user_type == 4) { // Dealer
+			$this->db->select('customers.id, customers.cust_code, wallet.amount as wallet_balance, dealer.full_name as dealer_name, distributor.full_name as distributor_name, customers.full_name, customers.mobile')
+				->from('customers')
+				->join('wallet', 'customers.cust_code = wallet.cust_code', 'left')
+				->join('users as dealer', 'dealer.user_id=customers.assigned_by_id', 'left')
+				->join('users as distributor', 'distributor.user_id=dealer.assigned_by_id', 'left')
+				->where('customers.is_deleted', '0')
+				->where('customers.assigned_by_id', $this->session->user['user_id'])
+				->order_by('customers.id', 'desc');
+		}
+
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function getInactiveAccountList()
+	{
+		$this->db->select('customers.id, customers.cust_code, wallet.amount as wallet_balance, dealer.full_name as dealer_name, distributor.full_name as distributor_name, customers.full_name, customers.mobile')
+			->from('customers')
+			->join('wallet', 'customers.cust_code = wallet.cust_code', 'left')
+			->join('users as dealer', 'dealer.user_id=customers.assigned_by_id', 'left')
+			->join('users as distributor', 'distributor.user_id=dealer.assigned_by_id', 'left')
+			->where('customers.is_deleted', '1')
+			->order_by('customers.id', 'desc');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function getTotalUser($acc_status = '0')
+	{
+		$this->db->select('count(id) as customers')
+			->from('customers')
+			->where('customers.is_deleted', $acc_status);
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	public function getTotalAmount($acc_status = '0')
+	{
+		$this->db->select('SUM(amount) as balance')
+			->from('wallet')
+			->join('customers', 'customers.cust_code = wallet.cust_code')
+			->where('customers.is_deleted', $acc_status);
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	public function getDetail($mobile)
+	{
+		$this->db->select('customers.full_name,customers.cust_code as id, wallet.amount as current_amount')
+			->from('customers')
+			->join('wallet', 'customers.cust_code = wallet.cust_code')
+			->where('customers.mobile', $mobile);
+		$query = $this->db->get();
+		return $query->row();
+	}
+}
---
+++ application/views/websitelayout/footer.php
@@ -0,0 +1,208 @@
+<h4 class="pby-us">POWERD BY SDBOSS.IN</h4>
+<a class="mp-btn" href="https://SDBOSS.in/SDBOSS.apk"><i>Matka Play</i></a>
+<a onclick="window.location.reload()" class="refresh-btn btm-btn-f">REFRESH </a>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org/",
+        "@type": "WebSite",
+        "additionalType": "BOSS MATKA ",
+        "name": "SDBOSS",
+        "alternateName": "DP MATKA    ",
+
+        "url": "https://SDBOSS.net/",
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://SDBOSS.net/search?q={search_term_string}SDBOSS",
+            "query-input": "required name=search_term_string"
+        }
+    }
+</script>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "additionalType": "BOSS Matka ",
+        "name": "SDBOSS",
+        "alternateName": "SATTA MATKA",
+        "url": "https://SDBOSS.net/",
+        "logo": "https://SDBOSS.net/index-images/logo.webp.pagespeed.ce.LrYCKmQQVo.webp",
+        "slogan": " Satta Matka SDBOSS Kalyan Matka Result ",
+        "sameAs": "https://SDBOSS.net/"
+    }
+</script>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": [{
+            "@type": "Question",
+            "name": "WHAT IS SATTA MATKA GAME?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Satta Matka Is A 60 Year Old Game Which Is Based And Played On Numbers By Matka Players To Earn Big Money"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT ARE THE TYPES OF MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Matka Is Played In Various Conditions But These 4 Are The Most Played Forms Of Matka. Open , Close , Jodi , Patti"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS THE FORMULA OF A SATTA CALCULATION?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Usually Matka Is A Game Of Luck But You Can Find Calculation By Running Line Of Current Game. Other U Can Follow The Dhanwarsha Chart As Well To Get The Accurate Number."
+            }
+        }, {
+            "@type": "Question",
+            "name": "CAN WE PLAY MATKA ONLINE?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "es There Are Many Website Like Matkabank.Com You Can Play Matka Online Without Any Problem And Without Going To Anywhere. You Can Play Here And Get Winning Amount Instantly"
+            }
+        }, {
+            "@type": "Question",
+            "name": "HOW CAN I FIND MATKA JODI?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Its A Bit Hard To Find Jodi But You Can Find Fix Matka Jodi To Get Big Profit Only On Our Date Fix Page.Kalyan Fix Jodi"
+            }
+        }, {
+            "@type": "Question",
+            "name": "HOW CAN I LEARN ABOUT SATTA MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Basically Matka Is An Easy Game. Its All About Numbers And Number Prediction. The Basic Thing About Matka Is 4 Things 1.Open- Select 0 To 9 Number To Bet On. If The Same Number Is Opened Then U Are The Winner Same Condition Is About The Close. Open Is The 1st Digit Of Jodi And Close Is The Second Digit."
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS PATTI / PANEL / PANNA OF MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Patti Is A Form Of 3 Digit Total Example - Patti 589 Here We Will Add These Number To Each Other Like 5+8+9=22 Here The 2nd Digit Will Be The Digit Of The Patti"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS AN EASY FORMULA TO GUESS SATTA MATKA NUMBERS?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Its Not Easy To Guess Satta Matka Number It Is The Work Of Matka Special Professors Called Guessers You Find There Guess Here.Visit Here"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHO IS SDBOSS?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Since 20 Years SDBOSS.Net Has Been An Idle For Every Matka Guesser. Everyone Wants To Be Like SDBOSS. SDBOSS Has An Outstanding Record Of Continuosly 7 Single Jodi Pass In Kalyan In 2019 July 2nd Week. He Is Working 24/7 For Matka Since 20 Years. He Is The One And Only In The Matka World. Every Player Of Matka Knows Him Respect Him. His Site Is Known As SDBOSS.Net. There Are Many Good Guessers As Well. You Can Visit This Website To Get Daliy Profit Of SDBOSS's Perfect Guessing. Its Not Hard To Win Money In Matka With The Help Of SDBOSS."
+            }
+        }]
+    }
+</script>
+<h4 class="pby-us">POWERD BY SDBOSS.IN</h4>
+<a class="mp-btn" href="https://SDBOSS.in/SDBOSS.apk"><i>Matka Play</i></a>
+<a onclick="window.location.reload()" class="refresh-btn btm-btn-f">REFRESH </a>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org/",
+        "@type": "WebSite",
+        "additionalType": "BOSS MATKA ",
+        "name": "SDBOSS",
+        "alternateName": "DP MATKA    ",
+
+        "url": "https://SDBOSS.net/",
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://SDBOSS.net/search?q={search_term_string}SDBOSS",
+            "query-input": "required name=search_term_string"
+        }
+    }
+</script>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "additionalType": "BOSS Matka ",
+        "name": "SDBOSS",
+        "alternateName": "SATTA MATKA",
+        "url": "https://SDBOSS.net/",
+        "logo": "https://SDBOSS.net/index-images/logo.webp.pagespeed.ce.LrYCKmQQVo.webp",
+        "slogan": " Satta Matka SDBOSS Kalyan Matka Result ",
+        "sameAs": "https://SDBOSS.net/"
+    }
+</script>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": [{
+            "@type": "Question",
+            "name": "WHAT IS SATTA MATKA GAME?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Satta Matka Is A 60 Year Old Game Which Is Based And Played On Numbers By Matka Players To Earn Big Money"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT ARE THE TYPES OF MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Matka Is Played In Various Conditions But These 4 Are The Most Played Forms Of Matka. Open , Close , Jodi , Patti"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS THE FORMULA OF A SATTA CALCULATION?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Usually Matka Is A Game Of Luck But You Can Find Calculation By Running Line Of Current Game. Other U Can Follow The Dhanwarsha Chart As Well To Get The Accurate Number."
+            }
+        }, {
+            "@type": "Question",
+            "name": "CAN WE PLAY MATKA ONLINE?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "es There Are Many Website Like Matkabank.Com You Can Play Matka Online Without Any Problem And Without Going To Anywhere. You Can Play Here And Get Winning Amount Instantly"
+            }
+        }, {
+            "@type": "Question",
+            "name": "HOW CAN I FIND MATKA JODI?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Its A Bit Hard To Find Jodi But You Can Find Fix Matka Jodi To Get Big Profit Only On Our Date Fix Page.Kalyan Fix Jodi"
+            }
+        }, {
+            "@type": "Question",
+            "name": "HOW CAN I LEARN ABOUT SATTA MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Basically Matka Is An Easy Game. Its All About Numbers And Number Prediction. The Basic Thing About Matka Is 4 Things 1.Open- Select 0 To 9 Number To Bet On. If The Same Number Is Opened Then U Are The Winner Same Condition Is About The Close. Open Is The 1st Digit Of Jodi And Close Is The Second Digit."
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS PATTI / PANEL / PANNA OF MATKA?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Patti Is A Form Of 3 Digit Total Example - Patti 589 Here We Will Add These Number To Each Other Like 5+8+9=22 Here The 2nd Digit Will Be The Digit Of The Patti"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHAT IS AN EASY FORMULA TO GUESS SATTA MATKA NUMBERS?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Its Not Easy To Guess Satta Matka Number It Is The Work Of Matka Special Professors Called Guessers You Find There Guess Here.Visit Here"
+            }
+        }, {
+            "@type": "Question",
+            "name": "WHO IS SDBOSS?",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "Since 20 Years SDBOSS.Net Has Been An Idle For Every Matka Guesser. Everyone Wants To Be Like SDBOSS. SDBOSS Has An Outstanding Record Of Continuosly 7 Single Jodi Pass In Kalyan In 2019 July 2nd Week. He Is Working 24/7 For Matka Since 20 Years. He Is The One And Only In The Matka World. Every Player Of Matka Knows Him Respect Him. His Site Is Known As SDBOSS.Net. There Are Many Good Guessers As Well. You Can Visit This Website To Get Daliy Profit Of SDBOSS's Perfect Guessing. Its Not Hard To Win Money In Matka With The Help Of SDBOSS."
+            }
+        }]
+    }
+</script>
+</body>
+<grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration>
+
+</html>
